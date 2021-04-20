beaa04036828472a641a33729beb696dbfbe644c
Moreno Gentili <brightsoul@tiscali.it>
2021-04-20 08:37:16 +0200

    Aggiunta la ricerca per ruolo

--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -25,12 +26,20 @@ namespace UserManagementUiDemo.Pages.Users
         [BindProperty(SupportsGet = true)]
         public string Search { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public Role? InRole { get; set; }
+
         public async Task<IActionResult> OnGetAsync()
         {
             IQueryable<ApplicationUser> userQuery = userManager.Users;
             if (Search is not null and not "")
             {
                 userQuery = userQuery.Where(user => EF.Functions.Like(user.FullName, $"%{Search}%") || EF.Functions.Like(user.Email, $"%{Search}%"));
+            }
+            if (InRole is not null)
+            {
+                string roleName = InRole.ToString();
+                userQuery = userQuery.Where(user => user.UserClaims.Any(claim => claim.ClaimType == ClaimTypes.Role && claim.ClaimValue == roleName));
             }
             Users = await userQuery.ToListAsync();
             ViewData["Title"] = $"Elenco utenti ({Users.Count})";
